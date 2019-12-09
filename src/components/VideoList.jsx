62515277540994dcb729fa2a4bb19fb89ff4ae68
abdelsalamshahlol <abdulsalamshahlol@gmail.com>
2019-12-09 17:14:40 +0100

    progress

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,3 +1,4 @@
+import exampleVideoData from '../data/exampleVideoData.js';
 var VideoList = () => (
   <div className="video-list">
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
@@ -17,3 +18,4 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
+console.log(exampleVideoData);
