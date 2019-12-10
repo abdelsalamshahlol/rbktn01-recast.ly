8eba5b8a28ddd13698a4850d3db27590fd9322c1
abdelsalamshahlol <abdulsalamshahlol@gmail.com>
2019-12-10 11:59:04 +0100

    fixed search youtube ajax request

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,24 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    method: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {
+      key: options.key,
+      q: options.query,
+      maxResults: options.max,
+      part: 'snippet',
+      videoEmbeddable: true,
+      type: 'video'
+      // forDeveloper: true
+    },
+    success: (data)=> {
+      console.log(data);
+      callback(data.items);
+    },
+    error: (e)=> {
+      console.error(e);
+    }
+  });
 };
 
 export default searchYouTube;
